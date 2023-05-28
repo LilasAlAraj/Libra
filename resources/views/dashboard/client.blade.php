92a338fa50b1da5c3d84029c6eaf5d184589883a
LilasAlAraj <lilas.alaraj.9@gmail.com>
2023-05-29 00:18:10 +0300

    11

--- resources/views/dashboard/client.blade.php
+++ resources/views/dashboard/client.blade.php
@@ -135,7 +135,7 @@
                                     <li id="role_permessionSettingsNav">
                                     </li>
 
-                                    <li><a href="../users/account_settings.html"
+                                    <li><a href="http://127.0.0.1:8000/account/setting"
                                             class="nav-link d-inline-flex  collapse-items">
                                             <span data-feather="tool" class="align-text-bottom"></span>
                                             إعدادات حسابي
