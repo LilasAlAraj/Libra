2792840b79a3b1f2657e192ada9f4e5a13272b09
LilasAlAraj <lilas.alaraj.9@gmail.com>
2023-06-12 20:46:07 +0300

    landing v1.1

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -18,8 +18,9 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm  d-flex justify-content-between">
+
+    <header class="sticky-top">
+        <nav class="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm ">
             <div class="container p-0 m-0 d-flex justify-space-between ">
                 <a class="navbar-brand  me-0 px-3 fs-6  col-1" href="#">
                     <div class="imgcontainer">
@@ -27,9 +28,10 @@
                     </div>
                 </a>
 
-                <button style="background-color:  rgb(87, 126, 155); padding: 0; margin-left: 2%" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon" ></span>
+                <button style="background-color:  rgb(87, 126, 155); padding: 0; margin-left: 2%" class="navbar-toggler"
+                    type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
@@ -37,7 +39,7 @@
                             <a class="nav-link" href="#">الصفحة الرئيسية</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">خدماتنا</a>
+                            <a class="nav-link" href="#features">ميزاتنا</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">الفريق</a>
@@ -45,10 +47,8 @@
                         <li class="nav-item">
                             <a class="nav-link" href="#">اتصل بنا</a>
                         </li>
-                        <li class="nav-item" style="text-align: right;">
-                            <button class="btn" id="login-btn">
-                                تسجيل الدخول
-                            </button>
+                        <li class="nav-item" id="login-nav-item" style="text-align: right;">
+
                         </li>
                     </ul>
                 </div>
@@ -58,18 +58,138 @@
     </header>
 
     <main>
-        <section>
+        <section class="landing justify-content-center align-items-center ">
             <div class="container">
-                <h1>مرحبًا بك في موقع محاماة</h1>
-                <p>نحن فريق محامين محترفين نقدم خدماتنا في مجال القانون والاستشارات القانونية.</p>
+                <div class="row">
+                    <div class="col-lg-6 col-md-6" id="landing-text">
+                        <div class="m-5">
+                            <h2>موقع العدل والعدالة</h2>
+                            <p>
+                                مرحبًا بك في صفحة إدارة مكتبنا التفاعلية! نحن فريق محاماة متخصص يقدم خدمات قانونية شاملة
+                                لعملائنا الكرام. نحن ندرك أهمية تنظيم وإدارة المكتب والقضايا، ولذلك نقدم لكم تجربة فريدة
+                                تجمع بين التكنولوجيا الحديثة والمهارات القانونية المتميزة.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 ">
+                        <div class="img-box rounded-circle position-relative">
+                            <img src="../../Img/Logo.jpg" style="width: 25%" class=" js-testimonial-img rounded-circle"
+                                alt="Logo">
+                            <h1 class="name">ليبرا</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="rgb(87, 126, 155)" fill-opacity="1"
+                    d="M0,128L60,154.7C120,181,240,235,360,224C480,213,600,139,720,128C840,117,960,171,1080,181.3C1200,192,1320,160,1380,144L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z">
+                </path>
+            </svg>
+        </section>
+
+
+        <section class="features fadeSection" id="features">
+            <div class="section-title">
+                <h3>ميزاتنا</h3>
+            </div>
+            <div class="container">
+                <div class="box">
+                    {{-- <h3>ما يميزنا</h3> --}}
+                    <div class="row box-row">
+                        <div class="feature-name col-lg-4 col-md-4 col-sm 6 fade_in_out" data-fade="100">
+                            <h4>إدارة المكتب</h4>
+
+                        </div>
+                        <div class="feature-description col-lg-8 col-md-8 col-sm 6 fade_in_out" data-fade="-100">
+                            <p>
+                                نسعى جاهدين لتقديم تجربة إدارة مكتبية محسّنة لعملائنا. يتيح موقعنا لكم الوصول إلى
+                                معلومات المكتب بسهولة، بما في ذلك ساعات العمل ومعلومات الاتصال. يمكنكم أيضًا جدولة
+                                المواعيد وإدارة تفاصيل المواعيد المحددة مع محاميكم.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row box-row">
+                        <div class="feature-name col-lg-4 col-md-4 col-sm 6 fade_in_out" data-fade="100">
+                            <h4>إدارة القضايا</h4>
+
+                        </div>
+                        <div class="feature-description col-lg-8 col-md-8 col-sm 6 fade_in_out" data-fade="-100">
+                            <p>
+
+                                يمكنكم إدارة القضايا والتخلص من الفوضى التي تخلفها المستندات الورقية.
+                                من خلال تخزين وتنظيم جميع المستندات القانونية الخاصة بقضاياكم بطريقة آمنة وسهلة الوصول.
+                                نساعدكم في الاحتفاظ بسجلاتكم القانونية بشكل منظم وفعال، مما يسهل عملية البحث عن
+                                المعلومات وتحليلها.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row box-row">
+                        <div class="feature-name col-lg-4 col-md-4 col-sm 6 fade_in_out" data-fade="100">
+                            <h4> البحث والبحث الذكي</h4>
+
+                        </div>
+                        <div class="feature-description col-lg-8 col-md-8 col-sm 6 fade_in_out" data-fade="-100">
+                            <p>
+                                يوفر موقعنا أدوات بحث متقدمة لمساعدتكم في العثور على المعلومات المهمة بسرعة ودقة.
+                                وباستخدام تقنيات الذكاء الاصطناعي، يمكنكم البحث عن المعلومات والأحكام
+                                ذات الصلة بقضيتكم بشكل فعال وفي وقت قصير.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row box-row">
+                        <div class="feature-name col-lg-4 col-md-4 col-sm 6 fade_in_out" data-fade="100">
+                            <h4>إدارة العملاء</h4>
+
+                        </div>
+                        <div class="feature-description col-lg-8 col-md-8 col-sm 6 fade_in_out" data-fade="-100">
+                            <p>
+                                نحن نهتم بعلاقتنا بعملائنا، ولذلك نوفر أدوات لإدارة بيانات العملاء بطريقة آمنة وسهلة.
+                                يمكنكم تسجيل معلومات العملاء، وتنظيم المستندات المتعلقة بهم،
+                                مما يسهل التواصل وتقديم خدمة متميزة.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row box-row">
+
+                        <div class="feature-name col-lg-4 col-md-4 col-sm 6 fade_in_out" data-fade="100">
+                            <h4>إدارة أفراد المكتب</h4>
+
+                        </div>
+                        <div class="feature-description col-lg-8 col-md-8 col-sm 6 fade_in_out " data-fade="-100">
+                            <p>
+                                نحن نولي اهتمامًا كبيرًا لأعضاء فريقنا في المكتب ونسعى جاهدين لتوفير الأدوات والموارد
+                                التي تمكّنهم من العمل بكفاءة وفعالية. نقدم وسائل لتعيين وتوزيع المهام بشكل منظم، وتتبع
+                                تقدم الأعضاء في تنفيذها وتحقيق الأهداف المحددة. كما نسهل التواصل والتعاون بين أفراد
+                                المكتب، حيث يمكنهم مشاركة المعلومات والمستندات بسهولة وبشكل آمن. نحن نسعى جاهدين لدعم
+                                نجاح أفراد المكتب وتطوير قدراتهم من خلال توفير بيئة عمل محفزة وداعمة.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row box-row">
+                        <div class="feature-name col-lg-4 col-md-4 col-sm 6 fade_in_out" data-fade="100">
+                            <h4>إدارة المهام</h4>
+
+                        </div>
+                        <div class="feature-description col-lg-8 col-md-8 col-sm 6 fade_in_out" data-fade="-100">
+                            <p>
+                                سواء كنتم ترغبون في تتبع مهام قضاياكم أو مهام المكتب اليومية، يوفر موقعنا أدوات لإدارة
+                                المهام بشكل فعال. يمكنكم تعيين المهام، تحديد المواعيد النهائية، وتتبع تقدم التنفيذ، مما
+                                يضمن تنظيمًا وتحقيق أهدافكم بكفاءة
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
+
+
+
+
+
     </main>
 
-    <footer>
-        <div class="container">
-            <p>جميع الحقوق محفوظة &copy; 2023</p>
-        </div>
+    <footer class="text-center">
+        <p>جميع الحقوق محفوظة © <span id="year"></span> <span class="logo">Libra</span></p>
     </footer>
 
 
