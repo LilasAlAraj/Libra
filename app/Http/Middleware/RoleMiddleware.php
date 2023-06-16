6a08af648c46996bcab881682452a60fe0e8c936
Boushra <tulipalmouhammad@gmail.com>
2023-06-16 15:28:00 +0300

    middleware V1.0

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware    
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (Auth::check() &&( Auth::user()->role_name === 'سكرتاريا' || Auth::user()->role_name=='مدير')   )
+        {
+            return $next($request);
+        }
+
+        elseif (Auth::check() &&  Auth::user()->role_name ==='محامي')
+
+        {
+            return redirect()->route('dashboard.lawyer');
+        }
+      
+        elseif (Auth::check() &&  Auth::user()->role_name ==='زبون')
+
+        {
+            return redirect()->route('dashboard.client');
+    
+        }
+}
+
+}
--- database/migrations/2023_05_29_172830_create_tasks_table.php
+++ database/migrations/2023_05_29_172830_create_tasks_table.php
@@ -9,20 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->string('name',50);
-            $table->text('description');
-            $table->date('start_date');
-            $table->date('end_date');
-            $table->string('Status');
-            $table->integer('Value_Status');
-            $table->string('priority');
-            $table->timestamps();
-        });
-    }
+        public function up(): void
+        {
+            Schema::create('tasks', function (Blueprint $table) {
+                $table->id();
+                $table->string('name',50);
+                $table->text('description');
+                $table->date('start_date');
+                $table->date('end_date');
+                $table->string('Status');
+                $table->integer('Value_Status');
+                $table->string('priority');
+                $table->timestamps();
+            });
+        }
 
     /**
      * Reverse the migrations.
